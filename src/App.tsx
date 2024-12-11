2ce35e2daf6af24dbbbbb64502723c7e8227960c
FbcGa <fabricio.alipazaga.pe@gmail.com>
2024-12-11 13:22:55 -0500

    input search added

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,41 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { NavBar } from "./components/navBar";
 import { CardCountrys } from "./components/cardCountrys";
+import { Search } from "lucide-react";
 
 function App() {
   const [countrys, setCountrys] = useState<Countrys[]>([]);
+  const [search, setSearch] = useState<string>("");
+
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((resp) => resp.json())
       .then((data) => setCountrys(data));
   }, []);
 
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCountry = search
+    ? countrys.filter((country) =>
+        country.name.common.toLocaleLowerCase().includes(search)
+      )
+    : countrys;
   return (
     <>
       <NavBar />
+      <form>
+        <Search />
+        <input
+          type="text"
+          value={search}
+          placeholder="Search for a country..."
+          onChange={handleSearch}
+        />
+      </form>
       <main>
-        <CardCountrys countrys={countrys} />
+        <CardCountrys countrys={filteredCountry} />
       </main>
     </>
   );
